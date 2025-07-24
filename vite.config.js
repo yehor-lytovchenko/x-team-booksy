e2be73d5511b726137f5b528fe207a5336fa2dfa
Oleg Shapoval <olegkazhan@gmail.com>
2025-07-24 17:49:01 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,48 +1,54 @@
-import { defineConfig } from 'vite';
-import { glob } from 'glob';
-import injectHTML from 'vite-plugin-html-inject';
-import FullReload from 'vite-plugin-full-reload';
-import SortCss from 'postcss-sort-media-queries';
+import { defineConfig } from "vite";
+import { glob } from "glob";
+import injectHTML from "vite-plugin-html-inject";
+import FullReload from "vite-plugin-full-reload";
+import postcssImport from "postcss-import";
+import postcssSortMediaQueries from "postcss-sort-media-queries";
 
 export default defineConfig(({ command }) => {
   return {
+    base: "/x-team-booksy/",
     define: {
-      [command === 'serve' ? 'global' : '_global']: {},
+      [command === "serve" ? "global" : "_global"]: {},
     },
-    root: 'src',
+    root: "src",
+    css: {
+      postcss: {
+        plugins: [
+          postcssImport(),
+          postcssSortMediaQueries({
+            sort: "mobile-first",
+          }),
+        ],
+      },
+    },
     build: {
       sourcemap: true,
       rollupOptions: {
-        input: glob.sync('./src/*.html'),
+        input: glob.sync("./src/*.html"),
         output: {
           manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
+            if (id.includes("node_modules")) {
+              return "vendor";
             }
           },
-          entryFileNames: chunkInfo => {
-            if (chunkInfo.name === 'commonHelpers') {
-              return 'commonHelpers.js';
+          entryFileNames: (chunkInfo) => {
+            if (chunkInfo.name === "commonHelpers") {
+              return "commonHelpers.js";
             }
-            return '[name].js';
+            return "[name].js";
           },
-          assetFileNames: assetInfo => {
-            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
-              return '[name].[ext]';
+          assetFileNames: (assetInfo) => {
+            if (assetInfo.name && assetInfo.name.endsWith(".html")) {
+              return "[name].[ext]";
             }
-            return 'assets/[name]-[hash][extname]';
+            return "assets/[name]-[hash][extname]";
           },
         },
       },
-      outDir: '../dist',
+      outDir: "../dist",
       emptyOutDir: true,
     },
-    plugins: [
-      injectHTML(),
-      FullReload(['./src/**/**.html']),
-      SortCss({
-        sort: 'mobile-first',
-      }),
-    ],
+    plugins: [injectHTML(), FullReload(["./src/**/**.html"])],
   };
 });
